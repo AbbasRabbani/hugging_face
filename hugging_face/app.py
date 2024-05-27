02a0ebefc7b454f4c44c7e1eb2dfc119d8711508
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2024-05-27 16:56:22 +0200

    ml-model_development-deployed-FASTAPI

---
+++ hugging_face/app.py
@@ -0,0 +1,38 @@
+#from flask import Flask, jsonify, render_template, request
+
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+
+from model import get_response
+
+#app = Flask(__name__)
+
+app = FastAPI()
+
+#### "" @app.route('/')
+"""def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    user_input = request.form['message']
+    response = get_response(user_input)
+    return jsonify({'response': response})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5010, debug=True) """
+
+
+class Message(BaseModel):
+    message: str
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the Simple Chatbot API"}
+
+@app.post("/predict")
+async def predict(message: Message):
+    response = get_response(message.message)
+    return {"response": response}
+
+
---
+++ hugging_face/model.py
@@ -0,0 +1,32 @@
+import os
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Load the Hugging Face token from environment variables
+# token = os.getenv("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")
+#
+# Load the tokenizer and model with the token
+#tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
+#model = AutoModelForCausalLM.from_pretrained("google/gemma-7b")
+
+#tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+#model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+
+tokenizer = AutoTokenizer.from_pretrained("sshleifer/tiny-gpt2")
+model = AutoModelForCausalLM.from_pretrained("sshleifer/tiny-gpt2")
+
+# Check if CUDA is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+def get_response(user_input):
+    # Tokenize the input text and move to GPU if available
+
+    
+    input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt").to(device)
+    # Generate a response
+    outputs = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    # Decode the generated response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
